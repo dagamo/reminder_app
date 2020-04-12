ffa017ebf37f133c1157e6788a8fb9f8818b6f13
David Garcia <garciamorales111.o.o.f@gmail.com>
2020-04-12 10:34:35 -0600

    Create the header

---
+++ src/containers/index.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect } from 'react';
+import { Box } from '@material-ui/core';
+import Header from './../components/header/index';
+
+const Index = () => {
+  
+	return (
+		<Box>
+			<Header />
+		</Box>
+	);
+};
+
+export default Index;
